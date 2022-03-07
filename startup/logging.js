f21c118332e05e1caf5c440ee45905a82e33de58
Bruno Ferreira <brunohlferreira@gmail.com>
2022-03-07 18:45:37 +0000

    Show unhandled exceptions on the console.

--- startup/logging.js
+++ startup/logging.js
@@ -4,6 +4,7 @@ require('express-async-errors');
 
 module.exports = function () {
     winston.exceptions.handle(
+        new winston.transports.Console(),
         new winston.transports.File({ filename: '../log/uncaughtExceptions.log' })
     );
     process.on('unhandledRejection', (ex) => {
@@ -13,6 +14,6 @@ module.exports = function () {
     winston.add(new winston.transports.MongoDB({ 
         db: 'mongodb://localhost/vidly',
         options: { useUnifiedTopology: true },
-        level: 'info'
+        level: 'error'
     }));
 }
