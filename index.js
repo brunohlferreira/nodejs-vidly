7fda192b31e3122e9ce2b2b58f0cf3254781e850
Bruno Ferreira <brunohlferreira@gmail.com>
2022-03-07 18:12:11 +0000

    Extracting routes.

--- index.js
+++ index.js
@@ -4,16 +4,10 @@ const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 const express = require('express');
 const app = express();
-const error = require('./middleware/error');
+require('./startup/routes')(app);
 require('express-async-errors');
 const winston = require('winston');
 require('winston-mongodb');
-const auth = require('./routes/auth');
-const customers = require('./routes/customers');
-const genres = require('./routes/genres');
-const movies = require('./routes/movies');
-const rentals = require('./routes/rentals');
-const users = require('./routes/users');
 
 winston.exceptions.handle(
     new winston.transports.File({ filename: './log/uncaughtExceptions.log' })
@@ -36,15 +30,6 @@ if (!config.get('jwtPrivateKey')) {
 mongoose.connect('mongodb://localhost/vidly')
     .then(() => console.log('Connected to MongoDB...'))
     .catch(err => console.error('Could not connect to MongoDB...', err));
-
-app.use(express.json());
-app.use('/api/auth', auth);
-app.use('/api/customers', customers);
-app.use('/api/genres', genres);
-app.use('/api/movies', movies);
-app.use('/api/rentals', rentals);
-app.use('/api/users', users);
-app.use(error);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
